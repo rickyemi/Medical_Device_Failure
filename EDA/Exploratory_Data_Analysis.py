225772ecba17e1d51632a2a1ceda0c4fc0e4a772
rickyemi <30478255+rickyemi@users.noreply.github.com>
2024-01-26 22:29:20 -0800

    Exploratory_Data_Analysis.py

---
+++ EDA/Exploratory_Data_Analysis.py
@@ -0,0 +1,248 @@
+# Libraries to help with reading and manipulating data
+import pandas as pd
+# Removes the limit for the number of displayed columns
+pd.set_option("display.max_columns", None)
+# Sets the limit for the number of displayed rows
+pd.set_option("display.max_rows", 100)
+# setting the precision of floating numbers to 3 decimal points
+pd.set_option("display.float_format", lambda x: "%.3f" % x)
+import numpy as np
+
+# libaries to help with data visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as msno
+
+# To ignore unnecessary warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+# Loading the dataset
+#connecting to google drive
+from google.colab import drive
+drive.mount('/content/drive')
+
+df_main = pd.read_csv("/content/drive/MyDrive/Previous_Projects/medical_device_failure.csv")
+
+# copying data to another variable to avoid any changes to original data
+data = df_main.copy()
+
+
+# Data Overview
+data.head()
+
+data.shape
+
+# check for missing values 
+percent_missing = data.isnull().sum() * 100 / len(data)
+missing_value_df = pd.DataFrame({'column_name': data.columns,
+                                 'percent_missing': percent_missing})
+
+missing_value_df
+
+# fix the missing values 
+
+data[['Velocity','Torque(Nm)',"Wear_Measure","Sensor_1","Sensor_2"]] = data[['Velocity','Torque(Nm)',"Wear_Measure","Sensor_1","Sensor_2"]].fillna(data[['Velocity','Torque(Nm)',"Wear_Measure","Sensor_1","Sensor_2"]].median())
+
+# statistical summary 
+
+data.describe(include = ['int','float']).T
+
+data.describe(include = 'object').T
+
+# User defined function to create barplots for categorical variable
+
+
+def categorical_barplot(data, feature, perc=False, n=None):
+    """
+    Barplot with percentage at the top
+
+    data: dataframe
+    feature: dataframe column
+    perc: whether to display percentages instead of count (default is False)
+    n: displays the top n category levels (default is None, i.e., display all levels)
+    """
+
+    total = len(data[feature])  # length of the column
+    count = data[feature].nunique()
+    if n is None:
+        plt.figure(figsize=(count + 2, 6))
+    else:
+        plt.figure(figsize=(n + 2, 6))
+
+    plt.xticks(rotation=60, fontsize=12)
+    ax = sns.countplot(
+        data=data,
+        x=feature,
+        palette="Paired",
+        order=data[feature].value_counts().index[:n],
+    )
+
+    for p in ax.patches:
+        if perc == True:
+            label = "{:.0f}%".format(
+                100 * p.get_height() / total
+            )  # percentage of each class of the category
+        else:
+            label = p.get_height()  # count of each level of the category
+
+        x = p.get_x() + p.get_width() / 2  # width of the plot
+        y = p.get_height()  # height of the plot
+
+        ax.annotate(
+            label,
+            (x, y),
+            ha="center",
+            va="center",
+            size=10,
+            xytext=(0, 5),
+            textcoords="offset points",
+        )  # annotate the percentage
+
+    plt.show()  # show the plot
+
+
+# User defined function to create histogram and boxplot for continuous variable
+
+def continuous_histogram_boxplot(data, feature, figsize=(12, 9), kde=False, bins=None):
+    """
+    Boxplot and histogram combined
+
+    data: dataframe
+    feature: dataframe column
+    figsize: size of figure (default (15,10))
+    kde: whether to show the density curve (default False)
+    bins: number of bins for histogram (default None)
+    """
+    f2, (ax_box2, ax_hist2) = plt.subplots(
+        nrows=2,  # Number of rows of the subplot grid= 2
+        sharex=True,  # x-axis will be shared among all subplots
+        gridspec_kw={"height_ratios": (0.25, 0.75)},
+        figsize=figsize,
+    )  # creating the 2 subplots
+    sns.boxplot(
+        data=data, x=feature, ax=ax_box2, showmeans=True, color="violet"
+    )  # boxplot will be created and a triangle will indicate the mean value of the column
+    sns.histplot(
+        data=data, x=feature, kde=kde, ax=ax_hist2, bins=bins
+    ) if bins else sns.histplot(
+        data=data, x=feature, kde=kde, ax=ax_hist2
+    )  # For histogram
+    ax_hist2.axvline(
+        data[feature].mean(), color="blue", linestyle="--"
+    )  # Add mean to the histogram
+    ax_hist2.axvline(
+        data[feature].median(), color="black", linestyle="-"
+    )  # Add median to the histogram
+
+
+# UDF
+def stacked_barplot(data, predictor, target):
+    """
+    Print the category counts and plot a stacked bar chart
+
+    data: dataframe
+    predictor: independent variable
+    target: target variable
+    """
+    count = data[predictor].nunique()
+    sorter = data[target].value_counts().index[-1]
+    tab1 = pd.crosstab(data[predictor], data[target], margins=True).sort_values(
+        by=sorter, ascending=False
+    )
+    print(tab1)
+    print("-" * 120)
+    tab = pd.crosstab(data[predictor], data[target], normalize="index").sort_values(
+        by=sorter, ascending=False
+    )
+    tab.plot(kind="bar", stacked=True, figsize=(count + 5, 5))
+    plt.legend(
+        loc="lower left", frameon=False,
+    )
+    plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
+    plt.show()
+
+
+### UDF to plot distributions wrt response variable
+
+
+def distribution_plot_wrt_target(data, predictor, target):
+
+    fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+
+    target_uniq = data[target].unique()
+
+    axs[0, 0].set_title("Distribution of target for target=" + str(target_uniq[0]))
+    sns.histplot(
+        data=data[data[target] == target_uniq[0]],
+        x=predictor,
+        kde=True,
+        ax=axs[0, 0],
+        color="teal",
+        stat="density",
+    )
+
+    axs[0, 1].set_title("Distribution of target for target=" + str(target_uniq[1]))
+    sns.histplot(
+        data=data[data[target] == target_uniq[1]],
+        x=predictor,
+        kde=True,
+        ax=axs[0, 1],
+        color="orange",
+        stat="density",
+    )
+
+    axs[1, 0].set_title("Boxplot w.r.t target")
+    sns.boxplot(data=data, x=target, y=predictor, ax=axs[1, 0], palette="gist_rainbow")
+
+    axs[1, 1].set_title("Boxplot (without outliers) w.r.t target")
+    sns.boxplot(
+        data=data,
+        x=target,
+        y=predictor,
+        ax=axs[1, 1],
+        showfliers=False,
+        palette="gist_rainbow",
+    )
+
+    plt.tight_layout()
+    plt.show()
+
+# univariate analysis for categorical variables 
+for col in data[['Device_Fail','Gear_Type']].columns:
+    categorical_barplot(data, col)
+
+# univariate analysis for continuous variables
+for col in data[['Velocity','Torque(Nm)','Wear_Measure','Sensor_1','Sensor_2']].columns:
+    continuous_histogram_boxplot(data, col)
+
+# Bivariate Analysis
+
+
+plt.figure(figsize=(10, 5))
+sns.heatmap(
+    data.corr(), annot=True, vmin=-1, vmax=1, fmt=".2f", cmap="Spectral"
+)
+plt.show()
+
+#Let's see how the target variable varies across all 4 Gear Types
+
+stacked_barplot(data, "Gear_Type", "Device_Fail")
+
+# Let's analyze the relation between Sensor_1 and Device_Fail.
+
+distribution_plot_wrt_target(data, "Sensor_1", "Device_Fail")
+
+# Outlier Detection 
+# outlier detection using boxplot
+continuous_variables = data.select_dtypes(include=np.number).columns.tolist()
+
+plt.figure(figsize=(12, 9))
+
+for i, variable in enumerate(continuous_variables):
+    plt.subplot(3, 3, i + 1)
+    plt.boxplot(data[variable], whis=1.5)
+    plt.tight_layout()
+    plt.title(variable)
+
+plt.show()
